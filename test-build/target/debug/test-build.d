ac7e638ae5d3e7f50601ea8f5e6c17aef04493c1
Samin Sarker <saminsarker05@gmail.com>
2024-06-24 23:16:59 -0400

    changed location of gitignore to root

---
+++ test-build/target/debug/test-build.d
@@ -0,0 +1,1 @@
+/Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/test-build/target/debug/test-build: /Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/test-build/src/main.rs
