8a3c4d916822c9f9e2638b492138fc88c1b29ec9
Samin Sarker <saminsarker05@gmail.com>
2024-06-24 23:35:42 -0400

    deleted artifacts

--- test-build/target/debug/test-build.d
+++
@@ -1,1 +0,0 @@
-/Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/test-build/target/debug/test-build: /Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/test-build/src/main.rs
