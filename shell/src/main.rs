e8b6a38af3cb73de2c434f8fc29f3ce6911a37d4
Samin Sarker <saminsarker05@gmail.com>
2024-06-27 15:48:00 -0400

    added seperate functions for piping and regular commands

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -12,39 +12,81 @@ fn main() {
     let mut _input = String::new();
     stdin().read_line(&mut _input).unwrap();
 
-    let mut line = _input.trim().split_whitespace();
-    if input.is_empty() { continue; }
+    let line = _input.trim();
+    if line.is_empty() { continue; }
 
-    let command = line.next().unwrap();
-    let args: Vec<&str> = line.collect();
-
-    match command {
-      "cd" => {
-        if args.is_empty() {
-          println!("cd: missing arg");
-          
-        } else {
-          let new_dir = args[0];
-          let result = env::set_current_dir(new_dir);
-          if let Err(e) = result {
-            println!("{}", e);
-          }
-        }
-      },
-      "exit" => { break; }
-      _ => {
-        let child = Command::new(command)
-          .args(args)
-          .spawn();
-        
-        // check that command is valid
-        match child {
-          Ok(mut child) => { let _ = child.wait(); }
-          Err(e) => { println!("{}", e); }
-        }
-      }
+    let cmds: Vec<&str> = line.split('|')
+      .map(|cmd| cmd.trim())
+      .collect();
+    
+    if cmds.len() == 1 { 
+      execute_command(cmds[0]); 
+    } else {
+      execute_pipe(cmds);
     }
+
+    
+    // let command = line.next().unwrap();
+    // let args: Vec<&str> = line.collect();
+
+    // match command {
+    //   "cd" => {
+    //     if args.is_empty() {
+    //       println!("cd: missing arg");
+          
+    //     } else {
+    //       let new_dir = args[0];
+    //       let result = env::set_current_dir(new_dir);
+    //       if let Err(e) = result {
+    //         println!("{}", e);
+    //       }
+    //     }
+    //   },
+    //   "exit" => { break; }
+    //   _ => {
+    //     let child = Command::new(command)
+    //       .args(args)
+    //       .spawn();
+        
+    //     // check that command is valid
+    //     match child {
+    //       Ok(mut child) => { let _ = child.wait(); }
+    //       Err(e) => { println!("{}", e); }
+    //     }
+    //   }
+    // }
 
   }
 
+}
+
+
+fn execute_command(command: &str) -> () {
+  let mut line = command.split_whitespace();
+  let cmd = line.next().unwrap();
+  let args: Vec<&str> = line.collect();
+
+  match cmd {
+    "cd" => {
+      if args.is_empty() {
+        println!("cd: missing arg");
+      } else {
+        let new_dir = args[0];
+        let result = env::set_current_dir(new_dir);
+        if let Err(e) = result { println!("{}", e); }
+      }
+    }
+    "exit" => { std::process::exit(0); }
+    _ => {
+      let child = Command::new(cmd)
+        .args(args)
+        .spawn();
+      match child {
+        Ok(mut child) => {
+          let _ = child.wait();
+        }
+        Err(e) => { println!("{}", e); }
+      }
+    }
+  }
 }
---
+++ shell/target/debug/shell.d
@@ -0,0 +1,1 @@
+/Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/shell/target/debug/shell: /Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/shell/src/main.rs
