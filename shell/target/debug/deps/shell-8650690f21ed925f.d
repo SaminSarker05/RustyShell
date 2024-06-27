910a5c3ace3367c2c92dddb6b7e240fad6fa34c4
Samin Sarker <saminsarker05@gmail.com>
2024-06-27 15:29:07 -0400

    deleted artifact

--- shell/target/debug/deps/shell-8650690f21ed925f.d
+++
@@ -1,5 +0,0 @@
-/Users/saminsarker/Documents/Documents - Samin’s MacBook Pro/Projects/RustyShell/shell/target/debug/deps/shell-8650690f21ed925f: src/main.rs
-
-/Users/saminsarker/Documents/Documents - Samin’s MacBook Pro/Projects/RustyShell/shell/target/debug/deps/shell-8650690f21ed925f.d: src/main.rs
-
-src/main.rs:
--- shell/target/debug/shell.d
+++
@@ -1,1 +0,0 @@
-/Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/shell/target/debug/shell: /Users/saminsarker/Documents/Documents\ -\ Samin’s\ MacBook\ Pro/Projects/RustyShell/shell/src/main.rs
