e7fcaa6857341439bffb541c2c453ec4b5bc301c
Samin Sarker <saminsarker05@gmail.com>
2024-06-27 15:08:04 -0400

    shell works without arguments

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,10 +1,22 @@
 use std::io::stdin;
+use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
-  let mut _input = String::new();
-  stdin().read_line(&mut _input).unwrap();
+  loop {
+    print!("> ");
+    let _ = io::stdout().flush();
 
-  let command = _input.trim();
-  println!("{}", command);
+    let mut _input = String::new();
+    stdin().read_line(&mut _input).unwrap();
+
+    let command = _input.trim();
+    
+    let mut child = Command::new(command)
+      .spawn()
+      .unwrap();
+
+    child.wait();
+  }
 
 }
